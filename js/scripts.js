b4b50d4becdc93fe38a33ed1d17b49314b19f04b
William Durand <will+git@drnd.me>
2018-11-26 11:03:08 +0100

    Minor tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
   if ($navbarBurgers.length > 0) {
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-        const target = el.dataset.target;
+    $navbarBurgers.forEach(($el) => {
+      $el.addEventListener('click', () => {
+        const target = $el.dataset.target;
         const $target = document.getElementById(target);
 
-        el.classList.toggle('is-active');
+        $el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
       });
     });
